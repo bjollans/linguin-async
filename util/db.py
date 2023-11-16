6aa29ba320eda27f9e5327ca3215df4308f33e2c
Bernard Jollans <jollans94@googlemail.com>
2023-11-16 16:57:05 +0530

    Fix format

--- util/db.py
+++ util/db.py
@@ -8,33 +8,36 @@ supabase = create_client(url, key)
 
 def get_messages_by_conversation_id(conversation_id):
     response = supabase \
-    .table('messages') \
-    .select("role, content") \
-    .eq("conversationId", conversation_id) \
-    .order("createdAt") \
-    .execute()
+        .table('messages') \
+        .select("role, content") \
+        .eq("conversationId", conversation_id) \
+        .order("createdAt") \
+        .execute()
     return response.data
+
 
 def get_conversation_by_id(conversation_id):
     response = supabase \
-    .table('conversations') \
-    .select("*") \
-    .eq("id", conversation_id) \
-    .single() \
-    .execute()
+        .table('conversations') \
+        .select("*") \
+        .eq("id", conversation_id) \
+        .single() \
+        .execute()
     return response.data
+
 
 def set_conversation_done(conversation_id):
     response = supabase \
-    .table('conversations') \
-    .update({"status": "done"}) \
-    .eq("id", conversation_id) \
-    .execute()
+        .table('conversations') \
+        .update({"status": "done"}) \
+        .eq("id", conversation_id) \
+        .execute()
     return response.data
+
 
 def insert_message(message):
     response = supabase \
-    .table('messages') \
-    .insert(message) \
-    .execute()
+        .table('messages') \
+        .insert(message) \
+        .execute()
     return response.data
