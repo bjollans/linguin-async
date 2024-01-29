3b86992455a05656d1d963c2dd46df0dd08e1210
Bernard Jollans <jollans94@googlemail.com>
2024-01-29 11:43:06 +0100

    Formatting

--- api/update_conversation.py
+++ api/update_conversation.py
@@ -2,15 +2,18 @@ import util.db as db
 import util.gpt as gpt
 from api.common_responses import success
 
+
 def _get_system_prompt(conversation_id):
     conversation = db.get_conversation_by_id(conversation_id)
     return conversation["systemPrompt"]
+
 
 def update_conversation(query):
     conversation_id = query["id"]
     messages = db.get_messages_by_conversation_id(conversation_id)
     if messages[-1]["role"] == "user":
-        answer = gpt.get_next_message(messages, _get_system_prompt(conversation_id))
+        answer = gpt.get_next_message(
+            messages, _get_system_prompt(conversation_id))
         answer["conversationId"] = conversation_id
         db.insert_message(answer)
     db.set_conversation_done(conversation_id)
